5fd30714d5d58ba76e7c7e8fafceb55e749c172b
Dana Pieluszczak <danajp@users.noreply.github.com>
2026-01-19 21:17:29 -0500

    Convert starship config to home-manager

--- home.nix
+++ home.nix
@@ -150,4 +150,45 @@
 
   home.file.".asdfrc".source = ./dot/asdfrc;
   home.file.".gemrc".source = ./dot/gemrc;
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      format = ''
+        $kubernetes$directory$git_branch$git_status$aws$direnv$nix_shell$cmd_duration$fill$time''${custom.local_time}$line_break$character
+      '';
+      kubernetes.disabled = false;
+      ruby.disabled = true;
+      nodejs.disabled = true;
+      gcloud.disabled = true;
+      golang.disabled = true;
+      python.disabled = true;
+      package.disabled = true;
+      terraform.disabled = true;
+      vagrant.disabled = true;
+      aws.region_aliases = {
+        us-east-1 = "use1";
+        us-west-2 = "usw2";
+        eu-central-1 = "euc1";
+        eu-west-1 = "euw1";
+        ap-southeast-2 = "apse2";
+        ap-southeast-4 = "apse4";
+      };
+      time = {
+        disabled = false;
+        utc_time_offset = "0";
+        time_format = "%T UTC";
+        format = "[$time]($style) ";
+      };
+      custom.local_time = {
+        description = "show local time";
+        when = true;
+        command = "date +'%T %Z'";
+        format = "[$output]($style)";
+      };
+      fill.symbol = " ";
+      direnv.disabled = false;
+    };
+  };
 }
